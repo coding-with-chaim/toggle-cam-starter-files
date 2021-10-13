5b69fd3a73d3e7fc66f90795e4d3da0443aa37e0
Chaim <calvinfriedman@Calvins-MacBook-Pro-2.local>
2021-10-13 16:55:18 -0400

    first commit

---
+++ public/scripts/room.js
@@ -0,0 +1,108 @@
+const peers = {};
+const roomId = window.location.pathname.split('/')[2];
+const userVideo = document.getElementById('user-video');
+const remoteVideo = document.getElementById('remote-video');
+const socket = io('/');
+
+function callOtherUsers(otherUsers, stream) {
+    otherUsers.forEach(userIdToCall => {
+        const peer = createPeer(userIdToCall);
+        peers[userIdToCall] = peer;
+        stream.getTracks().forEach(track => {
+            peer.addTrack(track, stream);
+        });
+    });
+}
+
+function createPeer(userIdToCall) {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    peer.onnegotiationneeded = () => userIdToCall ? handleNegotiationNeededEvent(peer, userIdToCall) : null;
+    peer.onicecandidate = handleICECandidateEvent;
+    peer.ontrack = (e) => {
+        remoteVideo.srcObject = e.streams[0];
+    }
+    return peer;
+}
+
+async function handleNegotiationNeededEvent(peer, userIdToCall) {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    const payload = {
+        sdp: peer.localDescription,
+        userIdToCall,
+    };
+
+    socket.emit('peer connection request', payload);
+}
+
+async function handleReceiveOffer({ sdp, callerId }, stream) {
+    const peer = createPeer(callerId);
+    peers[callerId] = peer;
+    const desc = new RTCSessionDescription(sdp);
+    await peer.setRemoteDescription(desc);
+
+    stream.getTracks().forEach(track => {
+        peer.addTrack(track, stream);
+    });
+
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+
+    const payload = {
+        userToAnswerTo: callerId,
+        sdp: peer.localDescription,
+    };
+
+    socket.emit('connection answer', payload);
+}
+
+function handleAnswer({ sdp, answererId }) {
+    const desc = new RTCSessionDescription(sdp);
+    peers[answererId].setRemoteDescription(desc).catch(e => console.log(e));
+}
+
+function handleICECandidateEvent(e) {
+    if (e.candidate) {
+        Object.keys(peers).forEach(id => {
+            const payload = {
+                target: id,
+                candidate: e.candidate,
+            }
+            socket.emit("ice-candidate", payload);
+        });
+    }
+}
+
+function handleReceiveIce({ candidate, from }) {
+    const inComingCandidate = new RTCIceCandidate(candidate);
+    peers[from].addIceCandidate(inComingCandidate);
+};
+
+function handleDisconnect(userId) {
+    delete peers[userId];
+    remoteVideo.remove();
+};
+
+socket.on('connect', async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    userVideo.srcObject = stream;
+    socket.emit('user joined room', roomId);
+
+    socket.on('all other users', (otherUsers) => callOtherUsers(otherUsers, stream));
+
+    socket.on("connection offer", (payload) => handleReceiveOffer(payload, stream));
+
+    socket.on('connection answer', handleAnswer);
+
+    socket.on('ice-candidate', handleReceiveIce);
+
+    socket.on('user disconnected', (userId) => handleDisconnect(userId));
+
+});
+
